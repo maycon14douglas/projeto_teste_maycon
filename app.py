56b2a51c6328af4785e7fc8787146ba8bf3884f4
Maycon Douglas <mayconfox14@gmail.com>
2025-07-16 20:36:48 -0300

    autoclick para abrir pasta nova

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-import pyautogui
-from time import sleep
+import pyautogui as pag
+pag.click(1522, 504, button='right', duration=1, interval=1)
+pag.click(1570, 780, button='left', duration=1, interval=1)
+pag.click(1250, 790, button='left', duration=1, interval=1)
+# Click na posição atual (com botão esquerdo)
 
-pyautogui.moveTo(1215, 368, duration=1)
-for i in range(1000):
-    sleep(0.5)
-    pyautogui.click()
+'''
+import pyautogui as pag
+pag.mouseDown(500, 1100, button='left')
+pag.mouseUp(500, 1100, button='left')
+pyautogui.click()
+pyautogui.click(button='left')
+pyautogui.click(button='right')
+pyautogui.click(button='middle')
+'''
+# meu codigo estava dando erro, procurei uma forma no google, achei esse modo pag bem mais facil de fazer.
+# por fim fui ajustando aos poucos
